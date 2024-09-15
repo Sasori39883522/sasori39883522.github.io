170571c9728f9810858794dfaf8f485e5f618e63
Sasori39883522 <sasori39883522@gmail.com>
2024-09-15 13:17:00 +0700

    feat: sync with hinatacampaign, add kage

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    if (self === parent) {
+        document.querySelector("body").classList.add("self");
+    } else {
+        document.querySelector("body").classList.add("child");
+    }
+
+    console.log("DOM loaded");
+    setTimeout(() => {
+        document
+            .querySelectorAll(
+                ".vjs-poster, .vjs-loading-spinner, .vjs-big-play-button"
+            )
+            .forEach((e) => e.remove());
+    }, 2000);
+
+    document.querySelectorAll(".hide-first").forEach(e => e.classList.remove('hide-first'));
+});
